6629c3c1598efb5c300f97666bfd1036c5c0c5b9
Amira-Sherif1 <s-amira.nassef@zewailcity.edu.eg>
2024-11-27 18:39:17 +0200

    initial identity

---
+++ OnlineCenter/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataAccess
+{
+    public class ApplicationDbContext :IdentityDbContext<IdentityUser>
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+        {
+        }
+
+    }
+}
